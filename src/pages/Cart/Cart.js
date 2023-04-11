dc6497b3cce509be3fb11f437ff0e28ac8f05439
Matthew Smart <matthewsmart13@gmail.com>
2023-04-11 15:49:28 -0500

    Added: decrementQuantity

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -14,6 +14,7 @@ const Cart = (props) => {
 							key={`cart-${item.id}`}
 							item={item}
 							incrementQuantity={props.incrementQuantity}
+							decrementQuantity={props.decrementQuantity}
 							getCartItem={props.getCartItem}
 						/>
 					);
