2d14a630f71ad5f7ef36412277f0720f0271e756
Matthew Smart <matthewsmart13@gmail.com>
2023-04-11 15:44:01 -0500

    Added: increment and decrement functions to comp

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -2,13 +2,25 @@ import React from "react";
 
 const CartItem = (props) => {
 	return (
-		<div>
+		<div data-cart-id={props.item.id}>
 			<img src={props.item.image} alt={props.item.title} />
 			<h3>{props.item.title}</h3>
 			<p>${props.item.price}</p>
-			<button>-</button>
+			<button
+				onClick={(e) => {
+					const itemId = e.currentTarget.parentNode.dataset.cartId;
+					props.decrementQuantity(props.getCartItem(itemId));
+				}}>
+				-
+			</button>
 			<p>{props.item.quantity}</p>
-			<button>+</button>
+			<button
+				onClick={(e) => {
+					const itemId = e.currentTarget.parentNode.dataset.cartId;
+					props.incrementQuantity(props.getCartItem(itemId));
+				}}>
+				+
+			</button>
 		</div>
 	);
 };
