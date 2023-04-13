1df9c098cc748c21f5b9329bfe6fa8d5ebeffdc9
Matthew Smart <matthewsmart13@gmail.com>
2023-04-13 13:34:33 -0500

    Added: conditional button text for render

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 const CartItem = (props) => {
+	const buttonText = props.item.quantity === 1 ? "Remove From Cart" : "-";
+
 	return (
 		<div data-cart-id={props.item.id}>
 			<img src={props.item.image} alt={props.item.title} />
@@ -11,7 +13,7 @@ const CartItem = (props) => {
 					const itemId = e.currentTarget.parentNode.dataset.cartId;
 					props.decrementQuantity(props.getCartItem(itemId));
 				}}>
-				-
+				{buttonText}
 			</button>
 			<p>{props.item.quantity}</p>
 			<button
