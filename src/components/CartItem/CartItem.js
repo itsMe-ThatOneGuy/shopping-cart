dd88ff9befed7abe0555ad3a179d85d6a3861fcb
Matthew Smart <matthewsmart13@gmail.com>
2023-04-14 00:19:50 -0500

    Changed: cartItem render / Added: handleOnChange

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,28 +1,57 @@
 import React from "react";
+import "./CartItem.css";
 
 const CartItem = (props) => {
 	const buttonText = props.item.quantity === 1 ? "Remove From Cart" : "-";
 
+	const handleOnChange = (e) => {
+		props.cartInputQuantity(
+			e.target.value,
+			props.getCartItem(e.currentTarget.parentNode.dataset.cartId)
+		);
+	};
+
 	return (
-		<div data-cart-id={props.item.id}>
-			<img src={props.item.image} alt={props.item.title} />
-			<h3>{props.item.title}</h3>
-			<p>${props.item.price}</p>
-			<button
-				onClick={(e) => {
-					const itemId = e.currentTarget.parentNode.dataset.cartId;
-					props.decrementQuantity(props.getCartItem(itemId));
-				}}>
-				{buttonText}
-			</button>
-			<p>{props.item.quantity}</p>
-			<button
-				onClick={(e) => {
-					const itemId = e.currentTarget.parentNode.dataset.cartId;
-					props.incrementQuantity(props.getCartItem(itemId));
-				}}>
-				+
-			</button>
+		<div className="cart-item">
+			<div className="cart-item-img">
+				<img src={props.item.image} alt={props.item.title} />
+			</div>
+			<div className="cart-item-right">
+				<div className="cart-item-details">
+					<h3>{props.item.title}</h3>
+					<p>${props.item.price}</p>
+				</div>
+				<div data-cart-id={props.item.id}>
+					<input
+						onChange={handleOnChange}
+						value={props.item.quantity}
+						type="number"></input>
+					<div
+						data-cart-id={props.item.id}
+						className="cart-item-control">
+						<button
+							onClick={(e) => {
+								const itemId =
+									e.currentTarget.parentNode.dataset.cartId;
+								props.decrementQuantity(
+									props.getCartItem(itemId)
+								);
+							}}>
+							{buttonText}
+						</button>
+						<button
+							onClick={(e) => {
+								const itemId =
+									e.currentTarget.parentNode.dataset.cartId;
+								props.incrementQuantity(
+									props.getCartItem(itemId)
+								);
+							}}>
+							+
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
